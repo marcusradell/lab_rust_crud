b382624260be7389604e203c9cb9d90ff61b73c5
Marcus Rådell <marcus@radell.net>
2024-01-07 15:38:35 +0100

    set default logging level to default

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 mod app_router;
+mod io;
 mod kits;
-mod logging;
 
 pub async fn lib() {
-    logging::init();
+    io::logging::init();
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
 
     let router = app_router::create().await;
 
